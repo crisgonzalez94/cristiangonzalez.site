99be667c0b50d22cdda5d19afe6656298963caaa
crisgonzalez94 <cristiannazarenogonzalez@gmail.com>
2020-08-04 19:57:54 -0300

    added header and fontawsome

---
+++ includes/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!--metadates-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Software Development">
+    <meta name="keywords" content="Programing ,  Freelance , Tutorials , Fiverr , Argentina , Uruguay">
+    <meta name="author" content="Cristian Gonzalez">
+
+    <!--Normalize-->
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!--Fontawesome-->
+    <script src="https://kit.fontawesome.com/98373b9dc2.js" crossorigin="anonymous"></script>
+
+    <title>Cristian Gonzalez</title>
+
+  </head>
+  <body>
+    
+    <header>
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Cristian Gonzalez</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Software</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Youtube Channel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+
+          </ul>
+
+          <!--social networks-->
+          <div class="form-inline my-2 my-lg-0">
+            <a class="nav-link" target="_blank" href="https://www.facebook.com/cristiannazarenogonzalez94">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a class="nav-link" target="_blank" href="https://www.instagram.com/developeronfreetimes/">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a class="nav-link" target="_blank" href="https://twitter.com/crisgonzalez94">
+              <i class="fab fa-twitter-square"></i>
+            </a>
+            <a class="nav-link" target="_blank"  href="https://www.youtube.com/channel/UCTfVTTAkvF7KF4CF7U64ORQ?view_as=subscriber">
+              <i class="fab fa-youtube-square"></i>
+            </a>
+          </div>
+
+        </div>
+      </nav>
+
+    </header>
--- index.php
+++ index.php
@@ -1,59 +1,9 @@
+<?php
+  require 'includes/header.php';
+?>
 
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <!--metadates-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Software Development">
-    <meta name="keywords" content="Programing ,  Freelance , Tutorials , Fiverr , Argentina , Uruguay">
-    <meta name="author" content="Cristian Gonzalez">
-
-    <!--Normalize-->
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-    <!--Bootstrap-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
-    <title>Cristian Gonzalez</title>
-
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Cristian Gonzalez</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Software</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Youtube Channel</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <a class="nav-link" href="#">English</a>
-          <a class="nav-link" href="#">Spanish</a>
-          <a class="nav-link" href="#">Portuguese</a>
-          <a class="nav-link" href="#">Italian</a>
-        </form>
-      </div>
-    </nav>
-
+    
 
     <!--jquery-->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
