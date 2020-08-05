dfd270a70ef9ea9701365486fe37f031948ae18f
crisgonzalez94 <cristiannazarenogonzalez@gmail.com>
2020-08-05 18:02:38 -0300

    added pages contact.php software.php youtubechannel.php

--- contact.php
+++ contact.php
@@ -2,6 +2,76 @@
     require 'includes/header.php';
 ?>
 
+<div class="container">
+    <br>
+    <h1>Contact me</h1>
+    <hr>
+
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#whatsapp" role="tab" aria-controls="whatsapp" aria-selected="true">Whatsapp</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#email" role="tab" aria-controls="Email" aria-selected="false">Email</a>
+        </li>
+    </ul>
+
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="whatsapp" role="tabpanel" aria-labelledby="home-tab">
+            <!--Content-->
+            <br>    
+            <form>
+
+                <div class="form-group">
+
+                    <label for="message">Put your message</label>
+                    <textarea class="form-control" id="message" rows="3"></textarea>
+                </div>
+
+                <button type="button" class="btn btn-primary btn-lg btn-block">
+                    Send message
+                </button>
+
+            </form>
+            <!------------>
+
+        </div>
+
+        <div class="tab-pane fade" id="email" role="tabpanel" aria-labelledby="profile-tab">
+            <!--Content-->
+            <br>    
+            <form>
+
+                <div class="form-group">
+
+                    <label for="affair">Affair</label>
+                    <input type="email" class="form-control" id="affair" aria-describedby="emailHelp">
+
+                    <label for="message">Put your message</label>
+                    <textarea class="form-control" id="message" rows="3"></textarea>
+                </div>
+
+                <button type="button" class="btn btn-primary btn-lg btn-block">
+                    Send message
+                </button>
+
+            </form>
+            <!------------>
+        </div>
+
+    </div>
+
+    <br>
+
+    <!--Other social networks-->
+    <div class="row">
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+    </div>
+
+</div>
+
 
 
 <!--This js code is for change class of header link-->
--- youtube-channel.php
+++ youtube-channel.php
@@ -2,6 +2,65 @@
     require 'includes/header.php';
 ?>
 
+<div class="container">
+    <br>
+    <h1>Videos</h1>
+    <hr>
+
+    <div class="card mb-3 bg-steel-blue">
+        <div class="row no-gutters">
+            <div class="col-md-4 col-sm-4">
+                <img src="assets/pictures/software/software-1.gif" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8 col-sm-8">
+                <div class="card-body">
+                    <h5 class="card-title white">Software title</h5>
+                    <p class="card-text white">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+                    <!--Download buttons-->
+                    <div class="row">
+
+                        <div class="col-md-3 col-sm-3">
+                            <button class="btn btn-outline-light btn-download toolstips" data-toggle="tooltip" data-placement="bottom" title="Download for windows 7 / 8.1 / 10">
+                                <i class="fab fa-windows"></i>
+                            </button>
+                        </div>
+
+                        <div class="col-md-3 col-sm-3">
+                            
+                            <button class="btn btn-outline-light btn-download toolstips" data-toggle="tooltip" data-placement="bottom" title="Download in PlayStore">
+                                <i class="fab fa-google-play"></i>
+                            </button>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-3">
+                            
+                            <button class="btn btn-outline-light btn-download toolstips" data-toggle="tooltip" data-placement="bottom" title="Download for osx 13.1 or higher">
+                                <i class="fab fa-apple"></i>
+                            </button>
+
+                        </div>
+
+                        <div class="col-md-3 col-sm-3">
+                                <button class="btn btn-outline-light btn-download toolstips" data-toggle="tooltip" data-placement="bottom" title="Download for debian distributions">
+                                <i class="fab fa-linux"></i>
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
 <!--This js code is for change class of header link-->
 <script>
     let youtube_channel_link = document.getElementById('youtube-channel-link');
